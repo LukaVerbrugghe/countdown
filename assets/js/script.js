d2485616b78c5defd5681b58f84d239d6d74b3a2
Luka Verbrugghe <luka.verbrugghe@student.howest.be>
2026-08-17 14:25:48 +0200

    Basic css for layout, font and colour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 import {renderTimers} from "./render.js";
 
 function init(){
-  renderTimers([{name: "Timer 1", months: 1, days: 2, hours: 3}, {name: "Timer 2", months: 4, days: 5, hours: 6}]);
+  renderTimers([{name: "Timer 1", months: 1, days: 2, hours: 3},{name: "Timer 2", months: 1, days: 2, hours: 3},{name: "Timer 3", months: 1, days: 2, hours: 3},{name: "Timer 4", months: 1, days: 2, hours: 3},{name: "Timer 5", months: 1, days: 2, hours: 3},{name: "Timer 600000000", months: 1, days: 2, hours: 3},{name: "Timer 700000", months: 1, days: 2, hours: 3},{name: "Timer 8", months: 1, days: 2, hours: 3},{name: "Timer 9", months: 1, days: 2, hours: 3},{name: "Timer 10000", months: 1, days: 2, hours: 3}]);
 }
 
 init();
