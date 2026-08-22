205c5c59e6965e24fe4310590d5e40a2b01c7ca9
Luka Verbrugghe <luka.verbrugghe@student.howest.be>
2026-08-22 18:35:29 +0200

    Update counters

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,15 +1,19 @@
 const TIMERS = [
   {
-    name: "Christmas",
-    date: new Date('12/25/2026')
-  },
-  {
     name: "My birthday",
     date: new Date('11/17/2026')
   },
   {
     name: "Yohan's birthday",
     date: new Date('12/11/2026')
+  },
+  {
+    name: "Calling my angel <3",
+    date: new Date('9/3/2026')
+  },
+  {
+    name: "Yohan's special 'shopping' day",
+    date: new Date('9/4/2026')
   }
 ]
 
