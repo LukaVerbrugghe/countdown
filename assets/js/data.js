0b8e8315f6ae9d69dd9add4f97347896b681e54e
Luka Verbrugghe <lotrdetailsys@gmail.com>
2026-09-18 12:08:59 +0200

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,13 +1,5 @@
 const TIMERS = [
   {
-    name: "My birthday",
-    date: new Date('11/17/2026')
-  },
-  {
-    name: "Yohan's birthday",
-    date: new Date('12/11/2026')
-  },
-  {
     name: "Finally seeing my angel <3",
     date: new Date('11/1/2026')
   }
